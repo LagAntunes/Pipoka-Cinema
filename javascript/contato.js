51735c86faa8fc66cfc131834c436288879faa5c
LagAntunes <lag151617@gmail.com>
2022-02-09 21:22:52 -0300

    criando as variáveis

---
+++ javascript/contato.js
@@ -0,0 +1,8 @@
+/* início da lógica JS da página de contato */
+    let mensagemOculta1 = document.getElementById("texto-aviso1");
+    let mensagemOculta2 = document.getElementById("texto-aviso2");
+    let mensagemOculta3 = document.getElementById("texto-aviso3");
+    let mensagemOculta4 = document.getElementById("texto-aviso4");
+    let botaoEnvio = document.getElementById("botao-submit");
+    
+/* fim da lógica JS da página de contato */
