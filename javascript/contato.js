ca62621bced480d75fa7ab4760f84b7155391885
LagAntunes <lag151617@gmail.com>
2022-02-10 19:39:55 -0300

    finalizando as lógicas dos inputs

--- javascript/contato.js
+++ javascript/contato.js
@@ -10,24 +10,67 @@
     let mensagemOculta4 = document.getElementById("texto-aviso4");
     let botaoEnvio = document.getElementById("botao-submit");
 
-    /*function clicaBotao() {
-        alert("teste")
-    }
-    botaoEnvio.addEventListener("click", clicaBotao)*/
-
-    botaoEnvio. onclick =  function confirmaEnvio() {
+    botaoEnvio.onclick =  function confirmaEnvio1() {
         event.preventDefault()
         if(nome.value == "") {
             mensagemOculta1.innerText = "Por favor informe o seu nome";
             mensagemOculta1.style.color = "red";
             mensagemOculta1.style.fontWeight = "bold";
             nome.style.borderColor = "red";
-        } else if(nome.value != "") {
+        } else if(nome.value != "")  {
             mensagemOculta1.innerText = "✔️";
             mensagemOculta1.style.fontSize = "1.2em";
-            nome.style.borderColor = "lightgreen"
+            nome.style.borderColor = "lightgreen";
+        }
+
+        if(email.value == "") {
+            mensagemOculta2.innerText = "Por favor informe o seu e-mail";
+            mensagemOculta2.style.color = "red";
+            mensagemOculta2.style.fontWeight = "bold";
+            email.style.borderColor = "red";
+        } else if(email.value != "")  {
+            mensagemOculta2.innerText = "✔️";
+            mensagemOculta2.style.fontSize = "1.2em";
+            email.style.borderColor = "lightgreen";
+        }
+
+        if(telefone.value == "") {
+            mensagemOculta3.innerText = "Por favor informe o seu telefone";
+            mensagemOculta3.style.color = "red";
+            mensagemOculta3.style.fontWeight = "bold";
+            telefone.style.borderColor = "red";
+        } else if(telefone.value != "")  {
+            mensagemOculta3.innerText = "✔️";
+            mensagemOculta3.style.fontSize = "1.2em";
+            telefone.style.borderColor = "lightgreen";
+        }
+
+        if(mensagem.value == "") {
+            mensagemOculta4.innerText = "Por favor escreva sua mensagem";
+            mensagemOculta4.style.color = "red";
+            mensagemOculta4.style.fontWeight = "bold";
+            mensagem.style.borderWidth = "2px";
+            mensagem.style.borderColor = "red";
+        } else if(mensagem.value != "")  {
+            mensagemOculta4.innerText = "✔️";
+            mensagemOculta4.style.fontSize = "1.2em";
+            mensagem.style.borderWidth = "2px";
+            mensagem.style.borderColor = "lightgreen";
         }
     }
-    
+
+    /*botaoEnvio.onclick =  function confirmaEnvio2() {
+        event.preventDefault()
+        if(email.value == "") {
+            mensagemOculta2.innerText = "Por favor informe o seu nome";
+            mensagemOculta2.style.color = "red";
+            mensagemOculta2.style.fontWeight = "bold";
+            email.style.borderColor = "red";
+        } else if(email.value != "")  {
+            mensagemOculta2.innerText = "✔️";
+            mensagemOculta2.style.fontSize = "1.2em";
+            email.style.borderColor = "lightgreen";
+        }
+    }*/
     
 /* fim da lógica JS da página de contato */
