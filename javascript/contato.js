3e5fc3bd4f5cd8c9368847cb094d44f298ea499a
LagAntunes <lag151617@gmail.com>
2022-02-09 21:46:35 -0300

    adicionando mais variaveis e testando funçoes

--- javascript/contato.js
+++ javascript/contato.js
@@ -1,8 +1,28 @@
 /* início da lógica JS da página de contato */
+    let nome = document.getElementById("nome");
+    let email = document.getElementById("email");
+    let telefone = document.getElementById("telefone");
+    let mensagem = document.getElementById("mensagem");
+
     let mensagemOculta1 = document.getElementById("texto-aviso1");
     let mensagemOculta2 = document.getElementById("texto-aviso2");
     let mensagemOculta3 = document.getElementById("texto-aviso3");
     let mensagemOculta4 = document.getElementById("texto-aviso4");
     let botaoEnvio = document.getElementById("botao-submit");
+
+    function clicaBotao() {
+        alert("teste")
+    }
+    botaoEnvio.addEventListener("click", clicaBotao)
+
+    /*function confirmaPreenchimento() {
+        if(botaoEnvio.click = true) {
+            if(nome.value == "") {
+                mensagemOculta1.innerText = "abacate"
+            }
+        }
+    }*/
+
+    
     
 /* fim da lógica JS da página de contato */
