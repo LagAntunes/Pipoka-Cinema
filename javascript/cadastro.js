543cbd9a215af9469057648d28bc6f3837a49b93
LagAntunes <lag151617@gmail.com>
2022-02-23 20:04:43 -0300

    adicionando API do CEP

--- javascript/cadastro.js
+++ javascript/cadastro.js
@@ -1,3 +1,24 @@
 /* início da lógica JS da página de cadastro */
     let botaoPremium = document.getElementById("botao-premium");
+
+
+
+    /* início da API do CEP */
+        const PreencherFormulario = (DadosDoCEP) => {
+            document.getElementById("endereco").value = DadosDoCEP.logradouro;
+            document.getElementById("bairro").value = DadosDoCEP.bairro;
+            document.getElementById("cidade").value = DadosDoCEP.localidade;
+            document.getElementById("estado").value = DadosDoCEP.uf;
+        }
+
+        let botaoCEP = document.getElementById("confirmaCEP");
+        botaoCEP.addEventListener("click", async() => {
+            event.preventDefault();
+            let CEP = document.getElementById("CEP").value;
+            const url = `http://viacep.com.br/ws/${CEP}/json/`;
+            const dados = await fetch(url);
+            const DadosDoCEP = await dados.json();
+            PreencherFormulario(DadosDoCEP);
+        })
+    /* fim da API do CEP */
 /* fim da lógica JS da página de cadastro */
